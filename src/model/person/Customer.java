da37e71159b76b2f0ad876a108369da0992a161b
batuanr <tuanpesi@gmail.com>
2021-10-14 20:53:06 +0700

    add class customer

---
+++ src/model/person/Customer.java
@@ -0,0 +1,25 @@
+package model.person;
+
+import java.time.LocalDate;
+
+public class Customer extends Person{
+    private String type;
+    private String id;
+
+    public Customer() {
+    }
+
+    public Customer(String name, LocalDate birthDate, String gender, String address, String phoneNumber, String type, String id) {
+        super(name, birthDate, gender, address, phoneNumber);
+        this.type = type;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
