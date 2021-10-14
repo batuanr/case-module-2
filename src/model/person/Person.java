6638597e14199224222ab41da45e711d82493b78
batuanr <tuanpesi@gmail.com>
2021-10-14 20:52:14 +0700

    add class person

---
+++ src/model/person/Person.java
@@ -0,0 +1,62 @@
+package model.person;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String name;
+    private LocalDate birthDate;
+    private String gender;
+    private String address;
+    private String phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String name, LocalDate birthDate, String gender, String address, String phoneNumber) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
